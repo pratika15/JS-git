ed77e3e2a2f8565a489115ef21519e241f0c6140
pratika15 <pratikabhanu@gmail.com>
2018-07-26 18:31:20 +0530

    javascript changes

---
+++ javascript/new.js
@@ -0,0 +1,18 @@
+var age =prompt("enter your age");
+var daysInAYear = 364.25;
+
+let calculate=()=>{
+days = age * daysInAYear;
+ return calculate();
+
+}
+
+let b=(days)=>{
+days = age * daysInAYear;
+
+
+ return days();
+
+
+}
+console.log( b(calculate) );
--- javascript/var_const_let.js
+++ javascript/var_const_let.js
@@ -1,26 +1,25 @@
 //var
 var a ="pratika"
+a= 1
 console.log(a);
-var a = 2
-console.log(a);//variable can assign different values
 
 
 //let
 
 let c ="patika"
 console.log(c);
-let c = 3
+ c = 3
 console.log(c);
 
 
 
 //const
 
-//const b ="pratika"
-//console.log(b);
-//const b =21
-//console.log(b);//show an error as b is already declared
-//
-//
-//
+const b ="pratika"
+console.log(b);
+ b =21
+console.log(b);//show an error as b is already declared
+
+
+
 
